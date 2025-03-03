922da85082ec8ba1a5dbe1177fa94f9153806918
Olivier Clerc <oclerc38@gmail.com>
2025-03-03 13:55:29 +0100

    chore: update names again

--- streamlit_app/__init__.py
+++ streamlit_app/__init__.py
@@ -3,6 +3,6 @@ Streamlit app for qualitative analysis using LLMs.
 This package contains modularized components of the Streamlit app.
 """
 
-from streamlit_app.app import QualitativeAnalysisApp
+from streamlit_app.app_core import QualitativeAnalysisApp
 
 __all__ = ["QualitativeAnalysisApp"]
